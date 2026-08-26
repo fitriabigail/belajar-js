011370ea03c58906c190ca0abf5db12f61f8f14f
fitriabigail <fitriabigail123@gmail.com>
2026-08-26 12:09:37 +0700

    update

---
+++ string.js
@@ -0,0 +1,13 @@
+let namaLengkap = "Fitri Abigail";
+
+namaLengkap = "Fitri Abigailll";
+console.log(namaLengkap);
+
+const tempatLahir = "Jakarta";
+console.log(tempatLahir);
+
+let text = "We are the so-called \"Vikings\" from the north.";
+console.log(text);
+
+console.log("ini length :", text.length);
+console.log(text.toUpperCase());
