e17ee24890cad670e4cda5df9a482e9b0da22407
fitriabigail <fitriabigail123@gmail.com>
2026-08-27 12:44:34 +0700

    update

--- array.js
+++ array.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-const arr = [];
+const array = [];
 const arrObject = [{}];
 
 
-const absen=["ala", "bigel", "bunga", "lifya"];
-=======
-const arr = [];
-const arrObject = [{}];
+const absenMahasiswa=["ala", "bigel", "bunga", "lifya"];
+console.log(absenMahasiswa[2]);
 
-
-const absen=["ala", "bigel", "bunga", "lifya"];
->>>>>>> 185990ac130d85eb6a36f439bd7b56f419297d35
-console.log(absen[2]);
