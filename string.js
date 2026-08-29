19c6406fd86ffb4e098ad202b6877964a381661b
fitriabigail <fitriabigail123@gmail.com>
2026-08-29 14:31:51 +0700

    update string

--- string.js
+++ string.js
@@ -12,3 +12,16 @@ console.log(text);
 
 console.log("ini length :", text.length);
 console.log(text.toUpperCase());
+
+let firstName = "John";
+let lastName = "Doe";
+
+let fullName = firstName + " " + lastName; 
+console.log(fullName); // John Doe
+
+//let name = "Alice";
+//console.log("Hello, " , name , "!"); // Hello, Alice!
+
+let name = "Alice";
+let age = 25;
+console.log("Name:", name, "Age:", age);
